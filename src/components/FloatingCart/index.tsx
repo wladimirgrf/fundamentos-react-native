06f3b5c7da559b1d669f9bc5fbdda28e83bec17d
Wladimir <wladimir.code@gmail.com>
2020-08-04 22:58:03 -0300

    feat: challenge solved

--- src/components/FloatingCart/index.tsx
+++ src/components/FloatingCart/index.tsx
@@ -24,15 +24,21 @@ const FloatingCart: React.FC = () => {
   const navigation = useNavigation();
 
   const cartTotal = useMemo(() => {
-    // TODO RETURN THE SUM OF THE PRICE FROM ALL ITEMS IN THE CART
+    const totalPrice = products.reduce((total, product) => {
+      const pricePerItem = product.price * product.quantity;
+      return total + pricePerItem;
+    }, 0);
 
-    return formatValue(0);
+    return formatValue(totalPrice);
   }, [products]);
 
   const totalItensInCart = useMemo(() => {
-    // TODO RETURN THE SUM OF THE QUANTITY OF THE PRODUCTS IN THE CART
+    const totalItens = products.reduce((total, product) => {
+      const quantityPerItem = product.quantity;
+      return total + quantityPerItem;
+    }, 0);
 
-    return 0;
+    return totalItens;
   }, [products]);
 
   return (
--- src/pages/Cart/index.tsx
+++ src/pages/Cart/index.tsx
@@ -39,23 +39,29 @@ const Cart: React.FC = () => {
   const { increment, decrement, products } = useCart();
 
   function handleIncrement(id: string): void {
-    // TODO
+    increment(id);
   }
 
   function handleDecrement(id: string): void {
-    // TODO
+    decrement(id);
   }
 
   const cartTotal = useMemo(() => {
-    // TODO RETURN THE SUM OF THE QUANTITY OF THE PRODUCTS IN THE CART
+    const totalPrice = products.reduce((total, product) => {
+      const pricePerItem = product.price * product.quantity;
+      return total + pricePerItem;
+    }, 0);
 
-    return formatValue(0);
+    return formatValue(totalPrice);
   }, [products]);
 
   const totalItensInCart = useMemo(() => {
-    // TODO RETURN THE SUM OF THE QUANTITY OF THE PRODUCTS IN THE CART
+    const totalItens = products.reduce((total, product) => {
+      const quantityPerItem = product.quantity;
+      return total + quantityPerItem;
+    }, 0);
 
-    return 0;
+    return totalItens;
   }, [products]);
 
   return (
